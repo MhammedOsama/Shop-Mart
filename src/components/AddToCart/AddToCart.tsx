c3b03209f41c3eb548d64fd4201ac101447191fd
MhammedOsama <mekhamhmd0@gmail.com>
2025-10-18 07:36:42 +0300

    Delete Wishlist files

--- src/components/AddToCart/AddToCart.tsx
+++ src/components/AddToCart/AddToCart.tsx
@@ -9,7 +9,6 @@ import { CartContext } from "../context/CartContext";
 import { AddToCartAction } from "@/app/(pages)/products/_action/AddToCart.action";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import AddToWishlist from "../AddtoWishlist/AddToWishlist";
 
 export default function AddToCart({ productId }: { productId: string }) {
   const [isLoading, setIsLoading] = useState(false);
@@ -44,7 +43,7 @@ export default function AddToCart({ productId }: { productId: string }) {
         )}{" "}
         Add To card
       </Button>
-      <AddToWishlist productId={productId} />
+      <HeartIcon />
     </CardFooter>
   );
 }
--- src/interfaces/wishlist.ts
+++
@@ -1,6 +0,0 @@
-import { Product } from "./product";
-export interface WishlistResponse {
-  status: string;
-  count: number;
-  data: Product[];
-}
